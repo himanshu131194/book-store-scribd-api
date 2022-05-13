c1b6fceb7e0e0f6045e5d769ce339063f61fb441
Himanshu Savita <himanshu131194@gmail.com>
2022-05-13 13:40:03 +0530

    Keep calm and commit

--- src/config/mongodb.config.spec.ts
+++ src/config/mongodb.config.spec.ts
@@ -17,6 +17,8 @@ describe('mongodbConfig', () => {
   });
 
   it('should contains uri key', async () => {
-    expect(config.uri).toBe('mongodb://localhost/blog');
+    expect(config.uri).toBe(
+      'mongodb+srv://bookstore:wBNyy0ThzZZMJgv6@bookstore.z7y3g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    );
   });
 });
